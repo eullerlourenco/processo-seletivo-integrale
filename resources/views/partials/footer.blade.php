87036df7e9df2eecb43726bd349ff0759b48f4b5
Euller Lourenço Silva <eullerlou321@gmail.com>
2025-04-03 22:02:08 -0300

    fix color

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white dark:bg-gray-900 dark:text-white w-full py-16 duration-300 ease-in-out">
+<footer class="bg-white dark:bg-gray-800 dark:text-white w-full py-16 duration-300 ease-in-out">
     <div class="w-full flex items-center justify-center">
         <p>
             Developed by
